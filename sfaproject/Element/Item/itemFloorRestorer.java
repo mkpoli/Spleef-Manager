19cae36eb7275a2171db05fab7728507dd69ff50
WeAthFolD <Cheetah77@qq.com>
2013-02-11 21:40:29 +0800

    Finally

--- sfaproject/Element/Item/itemFloorRestorer.java
+++ sfaproject/Element/Item/itemFloorRestorer.java
@@ -10,12 +10,13 @@ import net.minecraft.world.World;
 
 public class itemFloorRestorer extends Item {
 
-/*	private int x1;
-	private int z1;
-	private int x2;
-	private int z2;
-	private int ys; */
-	public boolean hasUsed=false;
+	private static int x1;
+	private static int z1;
+	private static int x2;
+	private static int z2;
+	private static int y1;
+	private static int y2;
+	public static boolean hasUsed=false;
 	public itemFloorRestorer(int par1) {
 		super(par1);
 		
@@ -40,27 +41,33 @@ public class itemFloorRestorer extends Item {
     public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2, World world, int x, int y, int z, int side, float par8, float par9, float par10)
     {
 		if (world.isRemote) return true;
-		
-		int x1,z2,x2,z1,z21,y1 = 0,y2 = 0;
 		if(hasUsed){
 			x2=x;
-			z21=z;
+			z2=z;
 			y2=y;
-			par2.sendChatToPlayer("选择了第一个选择点。");
-			hasUsed=false;
+			par2.sendChatToPlayer("Second selection point chosen.");	
+	    	if( y1 ==y2 ){
+	    		//TODO:用NBT保存数据，在聊天窗口中询问生成的区域名称
+	    		//最终保存用Command实现？
+	    		//SetSpleefBlock(world, x1, x2, z1, z2, y1);
+	    		//以上或许可以用SetArea/RestoreArea命令中的方法替代
+	    		hasUsed=false; //复位
+	    		x1=x2=y1=y2=z1=z2=0;
+	    		return true;
+	    	}else{
+	    		par2.sendChatToPlayer("Exception : Two points aren't at the same height.");
+	    		hasUsed=false;
+	    		return true;
+	    	}
  		}
 		else {
 			x1=x;
 			z1=z;
 			y1=y;
-			par2.sendChatToPlayer("选择了第二个选择点.");
+			par2.sendChatToPlayer("First selection point chosen.");
+			hasUsed=true;
 		} 
-    	if( !hasUsed || y1!=y2){
-    		return false;
-    	}
-		//	SetSpleefBlock(world, x1, x2, z1, z2, y);
-		//TODO:用NBT保存数据，在聊天窗口中询问生成的区域名称
-		//最终保存用Command实现？
+
         return true;
     }
 
