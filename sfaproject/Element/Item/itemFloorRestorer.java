be787a07a22948515f847748bc0869ef44e8e738
mkpoli <snowwolf1001@gmail.com>
2013-02-11 17:15:38 +0800

    2333333333333333333333

--- sfaproject/Element/Item/itemFloorRestorer.java
+++ sfaproject/Element/Item/itemFloorRestorer.java
@@ -10,6 +10,12 @@ import net.minecraft.world.World;
 
 public class itemFloorRestorer extends Item {
 
+/*	private int x1;
+	private int z1;
+	private int x2;
+	private int z2;
+	private int ys; */
+	
 	public itemFloorRestorer(int par1) {
 		super(par1);
 		
@@ -31,13 +37,31 @@ public class itemFloorRestorer extends Item {
         return 0;
     }
 	
-    public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
+    public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World world, int x, int y, int z, int side, float par8, float par9, float par10)
     {
+		if (world.isRemote) return true;
+		
+		/*
+		if(hasUsed){
+			x2=x;
+			z2=z;
+			System.out.println("第二个选择点" + x2 +","+ ys +","+ z2 );
+ 			}
+		else {
+			x1=x;
+			z1=z;
+			ys=y;
+			System.out.println("第一个选择点" + x1 +","+ y +","+ z1 );
+			} 
+			  */
+    	
+    //	SetSpleefBlock(world, x1, x2, z1, z2, y);
+    	
         return false;
     }
 
 	//Replacing Func
-	private boolean SetSpleefBlock(World world,int ax,int bx,int az,int bz,int y){
+	private boolean SetSpleefBlock(World world,int x1,int x2,int z1,int z2,int y){
 
 		return false;
 	}
