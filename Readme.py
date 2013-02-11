692e69196b62b745fcdb858b0bced3ed5b86694a
WeAthFoLD <Cheetah77@qq.com>
2013-02-11 02:15:49 -0800

    Create Readme.py
    Created a simple readme.xDDDDDD

---
+++ Readme.py
@@ -0,0 +1,22 @@
+SFA-Project(Current name changed to Spleef Manager) 
+is a Minecraft 1.4.7 SMP Mod Adding  useful features playing Spleef Game.
+
+Current Features:
+Blocks:
+SpleefBlock
+Specificly essy to be destroyed by shovels,no dropping.
+
+Items:
+ItemRestorerWand
+Used to select Spleef area and store it.
+Also used to restore area.
+
+Command:
+spfsetarea
+Used to set a Spleef Area.
+
+spfsetmaterial
+Used to set a specific material of the spleef arena.
+
+spfrestore
+Used to restore a Spleef area after the game.
